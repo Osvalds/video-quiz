134dd04228923903b2b7efc0ce1a9626f3c399ff
Osvalds Neiders <osvalds.neiders@gmail.com>
2018-11-22 21:29:14 +0200

    Switch to scss and add css reset.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player'
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
