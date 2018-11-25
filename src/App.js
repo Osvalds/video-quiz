969e2edb9ceb7d03154acf52d1f60916a5e6ff75
Osvalds Neiders <osvalds.neiders@gmail.com>
2018-11-25 20:34:57 +0200

    Get intro slide to loop

--- src/App.js
+++ src/App.js
@@ -7,31 +7,52 @@ import Question from "./components/Question";
 
 class App extends Component {
     state = {
-        playing: false,
+        playing: true,
         config: config.slides,
-        currentQuestion: 0
+        currentQuestion: 0,
+        muted: false
     };
 
     playPause = () => {
         this.setState({playing: !this.state.playing})
     };
 
+    toggleMuted = () => {
+        this.setState({muted: !this.state.muted})
+    };
+
+
     advanceToNextState = (nextState) => {
-        console.log("next-state", nextState)
         this.setState({currentQuestion: nextState})
     };
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log("snapshot: ", snapshot)
-    }
+    onFragmentStart = e => {
+        console.log("onstart", e);
+    };
+
+    onProgress = state => {
+        console.log('onProgress', state)
+        const {playedSeconds} = state;
+        const {videoEnd, loopStart} = this.state.config[this.state.currentQuestion];
+        console.log(loopStart, videoEnd);
+        if (playedSeconds >= videoEnd ) {
+            this.player.seekTo(parseFloat(loopStart))
+        }
+    };
+
+    ref = player => {
+        this.player = player
+    };
 
     render() {
-        const {playing, currentQuestion, config} = this.state;
+        const {playing, currentQuestion, config, muted} = this.state;
 
         return (
             <Fragment>
                 <ReactPlayer
-                    url='https://www.youtube.com/watch?v=PBwAxmrE194'
+                    ref={this.ref}
+
+                    url='https://youtu.be/hp0hHFa_GXU'
                     config={{
                         youtube: {
                             playerVars: {
@@ -46,10 +67,14 @@ class App extends Component {
                     playing={playing}
                     width={"auto"}
                     height={"auto"}
+                    muted={muted}
                     className={"player"}
+                    onStart={this.onFragmentStart}
+                    onProgress={this.onProgress}
                 />
                 <div className="controls">
                     <button onClick={this.playPause}>{playing ? "Pause" : "Play"}</button>
+                    <button onClick={this.toggleMuted}>{muted ? "Unmute" : "Mute"}</button>
                 </div>
                 <Question config={config[currentQuestion]}
                           advanceState={this.advanceToNextState}/>
