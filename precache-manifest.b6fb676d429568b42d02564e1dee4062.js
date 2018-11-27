66d145b2f10e2a950faf7db8571ec7fc06b6894c
Osvalds Neiders <osvalds.neiders@gmail.com>
2018-11-28 00:14:49 +0200

    Updates

---
+++ precache-manifest.b6fb676d429568b42d02564e1dee4062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "966215be50176f7688e54cd8f37e2a0a",
+    "url": "/video-quiz/static/media/subtitles.966215be.vtt"
+  },
+  {
+    "revision": "49252ecb60a48120861b",
+    "url": "/video-quiz/static/js/runtime~main.49252ecb.js"
+  },
+  {
+    "revision": "8e6f3959a7cdbe24c593",
+    "url": "/video-quiz/static/js/main.8e6f3959.chunk.js"
+  },
+  {
+    "revision": "f97a86353494bab5eb29",
+    "url": "/video-quiz/static/js/1.f97a8635.chunk.js"
+  },
+  {
+    "revision": "8e6f3959a7cdbe24c593",
+    "url": "/video-quiz/static/css/main.30ca67a2.chunk.css"
+  },
+  {
+    "revision": "c5bda71824451f30e63636696fb63f02",
+    "url": "/video-quiz/index.html"
+  }
+];
